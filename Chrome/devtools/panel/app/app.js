043728571544d2c2ac4a5aaff9e6c505107d2390
ToreiHispi <37494348+ToreiHispi@users.noreply.github.com>
2021-08-15 08:48:11 -0400

    Added Himetsuru Ichimonji crest

--- Chrome/devtools/panel/app/app.js
+++ Chrome/devtools/panel/app/app.js
@@ -6,7 +6,7 @@
   return exports.tohken.control = new Vue({
     el: '#app',
     data: {
-      version: '1.8.3',
+      version: '1.8.4',
       gaming: false,
       tab: 2,
       data: exports.tohken.data,
